b6d892a2e499fa5b99f7c3507d2575f892899aea
Yasmeen Taj <yasmeentaj.6041@gmail.com>
2025-12-08 02:14:57 +0530

    factorial  program using recursion

---
+++ factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fact(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factorial of %d = %d\n", n, fact(n));
+
+    return 0;
+}
